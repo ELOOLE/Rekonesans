f85a50422e9313dd750992b3dfe53aae5b329220
ELOOLE <75997374+ELOOLE@users.noreply.github.com>
2021-08-13 14:03:25 +0200

    Update html_parser.py

--- html_parser.py
+++ html_parser.py
@@ -6,7 +6,21 @@ file_html_new = file_html[:-5] + "_convert.html"
 open_file_html = open(file_html, "r")
 open_file_html_new = open(file_html_new, "w+")
 
-head = "<!DOCTYPE html>\n<html>\n<head>\n<style>\ntable, th, td {border: 1px solid black;}\nth {text-align: right; width: 5%;}\n</style>\n</head>\n<body>\n"
+head = '<!DOCTYPE html> \n'
+head += '<html>\n'
+head += '<head>\n'
+head += '<style>\n'
+head += 'table, th, td {'
+head += 'font-family: "Lucida Console", Monaco, monospace;'
+head += 'border: 1px solid #1C6EA4;'
+head += 'background-color: #EEEEEE;'
+head += 'width: 100%;'
+head += 'text-align: left;'
+head += 'border-collapse: collapse;}'
+head += 'th {text-align: right; width: 5%;}\n'
+head += '</style>\n'
+head += '</head>\n'
+head += '<body>\n'
 
 open_file_html_new.write(head)
 
@@ -26,6 +40,14 @@ for line in open_file_html:
         if(line.find("&lt;") > 0):
             i += 1
             line = line.replace("&lt;", "<")
+        else:
+            i -= 1
+
+    i = 1
+    while(i > 0):    
+        if(line.find("&amp;") > 0):
+            i += 1
+            line = line.replace("&amp;", "&")
         else:
             i -= 1
 
