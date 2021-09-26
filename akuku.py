db6ddae9deb1fb24a0d256c4942db8314d51a636
ELOOLE <75997374+ELOOLE@users.noreply.github.com>
2021-09-26 08:48:12 +0200

    Update akuku.py

--- akuku.py
+++ akuku.py
@@ -170,7 +170,7 @@ def f_odczyt_pliku_nmap(plik):
            
             # port 53, dns
             if(port == "53"):
-                zalecenia_dns = f"<i><b>dnsrecon -w -g -d {ip} --csv dnsrecon{ip}.csv</b></i>"
+                zalecenia_dns = f"<i><b>dnsrecon -w -g -d {ip} --csv /home/user/dnsrecon{ip}.csv</b></i> do zapisu, musi byc podana sciezna bezwzgledna inaczej nie zapisze"
                 data['host'].append({ip:{'dns':{'Dodatkowo&nbsp;można':f'<p style="color:red;">{zalecenia_dns}</p>\n'}}})
                 
             # port 67, 68 protocol: UDP, DHCP
