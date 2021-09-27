ddb249179a8cb93b08b176f29910b8319c654b51
ELOOLE <75997374+ELOOLE@users.noreply.github.com>
2021-09-27 08:52:11 +0200

    Update akuku.py

--- akuku.py
+++ akuku.py
@@ -217,7 +217,7 @@ def f_odczyt_pliku_nmap(plik):
         raport_html = open(path_plik_html, 'w')
         raport_html.write(json2html.convert(json = data, table_attributes='width="100%"', clubbing=True, encode=False, escape=True))
         raport_html.close()
-        html_parser.f_html_parser(path_plik_html)
+        f_html_parser(path_plik_html)
     except Exception as e:
         f_zapis_log("f_odczyt_pliku_nmap-raport_html",e,"error")
 
@@ -527,7 +527,51 @@ def f_policz_wiersze_w_pliku(path):
     otwarty_plik.close()
     # zwracam wynik
     return line_count
-    
+
+def f_html_parser(file_html):
+    file_html_new = file_html[:-5] + "_convert.html"
+
+    open_file_html = open(file_html, "r")
+    open_file_html_new = open(file_html_new, "w+")
+
+    head = '<!DOCTYPE html> \n'
+    head += '<html>\n'
+    head += '<head>\n'
+    head += '<style>\n'
+    head += 'table, th, td {'
+    head += 'font-family: "Lucida Console", Monaco, monospace;'
+    head += 'border: 1px solid #1C6EA4;'
+    head += 'background-color: #EEEEEE;'
+    head += 'width: 100%;'
+    head += 'text-align: left;'
+    head += 'border-collapse: collapse;}'
+    head += 'th {text-align: right; width: 5%;}\n'
+    head += '</style>\n'
+    head += '</head>\n'
+    head += '<body>\n'
+
+    open_file_html_new.write(head)
+
+    for line in open_file_html:
+        line = line.replace("&gt;", ">")
+        line = line.replace("&lt;", "<")
+        line = line.replace("&amp;", "&")
+        line = line.replace('&quot;', '"')
+        line = line.replace('&#x27;',"'")
+        line = line.replace('"<','')
+        line = line.replace('\\r\\n','<br />')
+        line = line.replace('\\n','<br />')
+        line = line.replace('</td>','</td>\n')
+        line = line.replace('</table>','</table>\n')
+        
+        open_file_html_new.write(line)
+
+    foot = "\n</body>\n</html>"    
+    open_file_html_new.write(foot)
+
+    open_file_html.close()
+    open_file_html_new .close()
+
 if __name__ == '__main__':
     '''MAIN'''
     parser = argparse.ArgumentParser()
