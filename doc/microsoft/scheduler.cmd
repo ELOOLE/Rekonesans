7505458b515721bd3da1e3238f6b116eae6f5d45
ELOOLE <75997374+ELOOLE@users.noreply.github.com>
2022-08-24 11:05:56 +0200

    Update scheduler.cmd

--- doc/microsoft/scheduler.cmd
+++ doc/microsoft/scheduler.cmd
@@ -1,3 +1,3 @@
 # dodanie do harmonogramu zadań 
 # na co 20 minut wykonywać wskazany skrypt
-schtasks /create /sc minute /mo 20 /tn "Security Script" /tr c:\users\user\Downloads\shell.ps1
+schtasks /create /sc minute /mo 20 /tn "Security Script" /tr "powershell -File c:\users\user\Downloads\shell.ps1" /ru System
