dfb99843caecce4d483dd620e7b6ef2666880519
ELOOLE <75997374+ELOOLE@users.noreply.github.com>
2022-08-24 10:11:14 +0200

    Update Invoke-WebRequest.ps1

--- doc/microsoft/Invoke-WebRequest.ps1
+++ doc/microsoft/Invoke-WebRequest.ps1
@@ -1,2 +1,10 @@
 # ściągnięcie pliku z sieci odpowiednik polecenia w Linux-ie wget
 Invoke-WebRequest -Uri https://github.com/main.zip -OutFile c:\users\user\Downloads\main.zip
+
+# w formie skryptu
+# plik zrodlowy
+$source = 'http://speedtest.tele2.net/10MB.zip'
+# plik docelowy
+$destination = 'c:\dload\10MB.zip'
+# skladnia polecenia
+Invoke-WebRequest -Uri $source -OutFile $destination
